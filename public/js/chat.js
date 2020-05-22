1103e56fb9365feb9c354a5ca67727b52632e51c
Mitchell Goodwin <mgoodwin777@gmail.com>
2020-05-22 10:54:56 -0500

    Added rooms to the chat

--- public/js/chat.js
+++ public/js/chat.js
@@ -9,6 +9,9 @@ const $messages = document.querySelector('#messages')
 const messageTemplate = document.querySelector('#message-template').innerHTML
 const linkTemplate = document.querySelector('#link-template').innerHTML
 
+// Options
+const { username, room } = Qs.parse(location.search, { ignoreQueryPrefix: true })
+
 socket.on('message', (message) => {
     console.log(message)
     const html = Mustache.render(messageTemplate, { 
@@ -59,3 +62,5 @@ $locationButton.addEventListener('click', () => {
         })
     })
 })
+
+socket.emit('join', { username, room })
